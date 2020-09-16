6c2331aebd8250f0da8e4d8771d3b124a8b3d919
kevinrobinson <kevin.robinson.0@gmail.com>
2020-09-16 13:26:25 -0400

    Fix whitescreen bug when disabling embedding projector

--- lit_nlp/client/core/modules.ts
+++ lit_nlp/client/core/modules.ts
@@ -181,7 +181,9 @@ export class LitModules extends LitElement {
 
   renderComponentType(configGroup: RenderConfig[]) {
     const modulesInGroup = configGroup.length > 1;
-    const duplicateAsRow = configGroup[0].moduleType.duplicateAsRow;
+    const duplicateAsRow = (configGroup.length > 0)
+      ? configGroup[0].moduleType.duplicateAsRow
+      : false;
     const componentsHTML = configGroup.map(
         config => this.renderModule(config, modulesInGroup && !duplicateAsRow));
     if (modulesInGroup) {
