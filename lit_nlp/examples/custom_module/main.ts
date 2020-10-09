6e356ccd39f24f0f4084c11da71a8e4076582575
James Wexler <jwexler@google.com>
2020-10-09 14:12:43 -0400

    test custom front end and server

---
+++ lit_nlp/examples/custom_module/main.ts
@@ -0,0 +1,26 @@
+/**
+ * Test frontend.
+ */
+
+// Imports the main LIT App web component, which is declared here then attached
+// to the DOM as <lit-app>
+import '../client/app/app';
+
+import {app} from '../client/core/lit_app';
+import {LAYOUTS} from '../client/default/layout';
+
+import {ClassificationModule} from '../client/modules/classification_module';
+import {DataTableModule} from '../client/modules/data_table_module';
+import {DatapointEditorModule} from '../client/modules/datapoint_editor_module';
+import {PotatoModule} from './potato';
+
+LAYOUTS['potato'] = {
+  components: {
+    'Main': [DatapointEditorModule, ClassificationModule],
+    'Data': [DataTableModule, PotatoModule],
+  },
+};
+
+// Initialize the app core logic, using the specified declared layout.
+app.initialize(LAYOUTS);
+
---
+++ lit_nlp/examples/custom_module/potato.ts
@@ -0,0 +1,41 @@
+/**
+ * @fileoverview They're red, they're white, they're brown,
+ * they get that way under-ground...
+ */
+
+// tslint:disable:no-new-decorators
+import {app} from '../lit_nlp/client/core/lit_app';
+import {LitModule} from '../lit_nlp/client/core/lit_module';
+import {ModelsMap, Spec} from '../lit_nlp/client/lib/types';
+import {customElement, html, property} from 'lit-element';
+import {styleMap} from 'lit-html/directives/style-map';
+
+/** Custom LIT module. */
+@customElement('potato-module')
+export class PotatoModule extends LitModule {
+  static title = 'Potato';
+  static numCols = 4;
+  static template = () => {
+    return html`<potato-module></potato-module>`;
+  };
+
+  render() {
+    const style = styleMap({'width': '100%', 'height': '100%'});
+    // clang-format off
+    return html`
+      <a href="https://potato.io/">
+        <img src="static/potato.svg" style=${style}>
+      </a>`;
+    // clang-format on
+  }
+
+  static shouldDisplayModule(modelSpecs: ModelsMap, datasetSpec: Spec) {
+    return true;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'potato-module': PotatoModule;
+  }
+}
