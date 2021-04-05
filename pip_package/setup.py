f89751b4f4b0f091561e614e0392dcd8b08ecf53
James Wexler <jwexler@google.com>
2021-04-05 07:14:54 -0700

    Update pip package version
    PiperOrigin-RevId: 366794351

--- pip_package/setup.py
+++ pip_package/setup.py
@@ -39,7 +39,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lit-nlp",
-    version="0.2",
+    version="0.3",
     description="Language Interpretability Tool.",
     long_description=long_description,
     long_description_content_type="text/markdown",
