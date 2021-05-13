f071b4e686dd764ada8a49739882384494de51eb
Googler <no-reply@google.com>
2021-05-13 09:36:05 -0700

    Extend TFXModel to serialize TF examples and pass them as inputs to the model since TFX models expect this as input. Also add dependency for tf_text, which is commonly used for NLP TF models.
    PiperOrigin-RevId: 373594674

--- lit_nlp/components/tfx_model.py
+++ lit_nlp/components/tfx_model.py
@@ -5,8 +5,28 @@ from lit_nlp.api import model as lit_model
 from lit_nlp.api import types as lit_types
 
 import tensorflow as tf
+import tensorflow_text as tf_text  # pylint: disable=unused-import
 
 _SERVING_DEFAULT_SIGNATURE = 'serving_default'
+
+
+# TODO(b/188036366): Revisit the assumed mapping between input values and
+# TF.Examples.
+def _inputs_to_serialized_example(input_dict: lit_types.JsonDict):
+  """Converts the input dictionary to a serialized tf example."""
+  feature_dict = {}
+  for k, v in input_dict.items():
+    if not isinstance(v, list):
+      v = [v]
+    if isinstance(v[0], int):
+      feature_dict[k] = tf.train.Feature(int64_list=tf.train.Int64List(value=v))
+    elif isinstance(v[0], float):
+      feature_dict[k] = tf.train.Feature(float_list=tf.train.FloatList(value=v))
+    else:
+      feature_dict[k] = tf.train.Feature(
+          bytes_list=tf.train.BytesList(value=[bytes(i, 'utf-8') for i in v]))
+  result = tf.train.Example(features=tf.train.Features(feature=feature_dict))
+  return result.SerializeToString()
 
 
 class TFXModel(lit_model.Model):
@@ -20,12 +40,22 @@ class TFXModel(lit_model.Model):
     self._input_spec = input_spec
     self._output_spec = output_spec
 
-  def predict_minibatch(self, inputs: List[lit_types.JsonDict]) -> Iterator[
-      lit_types.JsonDict]:
+  def predict_minibatch(
+      self, inputs: List[lit_types.JsonDict]) -> Iterator[lit_types.JsonDict]:
     for i in inputs:
+      filtered_inputs = {k: v for k, v in i.items() if k in self._input_spec}
       result = self._model.signatures[self._signature](
-          **{k: tf.reshape(tf.constant(v), [1, -1]) for k, v in i.items()})
-      result = {k: tf.squeeze(v).numpy() for k, v in result.items()}
+          tf.constant([_inputs_to_serialized_example(filtered_inputs)]))
+      result = {
+          k: tf.squeeze(v).numpy().tolist()
+          for k, v in result.items()
+          if k in self._output_spec
+      }
+      for k, v in result.items():
+        # If doing Multiclass Prediction for a Binary Classifier.
+        if (isinstance(self._output_spec[k], lit_types.MulticlassPreds) and
+            not isinstance(v, list)):
+          result[k] = [1 - v, v]
       yield result
 
   def input_spec(self) -> lit_types.Spec:
--- lit_nlp/components/tfx_model_test.py
+++ lit_nlp/components/tfx_model_test.py
@@ -3,7 +3,6 @@ import tempfile
 
 from lit_nlp.api import types as lit_types
 from lit_nlp.components import tfx_model
-import numpy as np
 import tensorflow as tf
 
 
@@ -12,9 +11,14 @@ class TfxModelTest(tf.test.TestCase):
   def setUp(self):
     super(TfxModelTest, self).setUp()
     self._path = tempfile.mkdtemp()
-    input_layer = tf.keras.layers.Input(shape=(1,), dtype=tf.float32,
-                                        name='input_0')
-    output_layer = tf.keras.layers.Dense(1, name='output_0')(input_layer)
+    input_layer = tf.keras.layers.Input(
+        shape=(1), dtype=tf.string, name='example')
+    parsed_input = tf.io.parse_example(
+        tf.reshape(input_layer, [-1]),
+        {'input_0': tf.io.FixedLenFeature([1], dtype=tf.float32)})
+    output_layer = tf.keras.layers.Dense(
+        1, name='output_0')(
+            parsed_input['input_0'])
     model = tf.keras.Model(input_layer, output_layer)
     model.compile(
         optimizer=tf.keras.optimizers.Adam(lr=.001),
@@ -23,15 +27,19 @@ class TfxModelTest(tf.test.TestCase):
 
   def testTfxModel(self):
     input_spec = {'input_0': lit_types.Scalar()}
-    output_spec = {'output_0': lit_types.RegressionScore(parent='input_0')}
-    lit_model = tfx_model.TFXModel(self._path,
-                                   input_spec=input_spec,
-                                   output_spec=output_spec)
+    output_spec = {
+        'output_0':
+            lit_types.MulticlassPreds(vocab=['0', '1'], parent='input_0')
+    }
+    lit_model = tfx_model.TFXModel(
+        self._path, input_spec=input_spec, output_spec=output_spec)
     result = list(lit_model.predict([{'input_0': 0.5}]))
     self.assertLen(result, 1)
     result = result[0]
     self.assertListEqual(list(result.keys()), ['output_0'])
-    self.assertIsInstance(result['output_0'], np.float32)
+    self.assertLen(result['output_0'], 2)
+    self.assertIsInstance(result['output_0'][0], float)
+    self.assertIsInstance(result['output_0'][1], float)
     self.assertDictEqual(lit_model.input_spec(), input_spec)
     self.assertDictEqual(lit_model.output_spec(), output_spec)
 
