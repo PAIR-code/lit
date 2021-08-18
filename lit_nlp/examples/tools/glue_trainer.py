d44e983155c122ee1dcd6eb6f3dee47db210f8ed
Ian Tenney <iftenney@google.com>
2021-08-18 14:06:08 -0700

    Option to write intermediate checkpoints from GLUE trainer.
    PiperOrigin-RevId: 391605793

--- lit_nlp/examples/tools/glue_trainer.py
+++ lit_nlp/examples/tools/glue_trainer.py
@@ -35,6 +35,11 @@ flags.DEFINE_string("task", "sst2", "Name of task to fine-tune on.")
 flags.DEFINE_string("train_path", "/tmp/hf_demo",
                     "Path to save fine-tuned model.")
 
+flags.DEFINE_integer(
+    "num_epochs", 3, "Number of epochs to train for.", lower_bound=1)
+flags.DEFINE_bool("save_intermediates", False,
+                  "If true, save intermediate weights after each epoch.")
+
 FLAGS = flags.FLAGS
 
 
@@ -47,7 +52,34 @@ def history_to_dict(keras_history):
   }
 
 
-def train_and_save(model, train_data, val_data, train_path):
+class EpochSaverCallback(tf.keras.callbacks.Callback):
+  """Save model at the beginning of training and after every epoch.
+
+  Similar to tf.keras.callbacks.ModelCheckpoint, but this allows us to specify
+  a custom save fn to call, such as the HuggingFace model.save() which writes
+  .h5 files and config information.
+  """
+
+  def __init__(self, save_path_base: str, save_fn=None):
+    super().__init__()
+    self.save_path_base = save_path_base
+    self.save_fn = save_fn or self.model.save
+
+  def on_train_begin(self, logs=None):
+    self.on_epoch_end(-1, logs=logs)  # write epoch-0
+
+  def on_epoch_end(self, epoch, logs=None):
+    # Save path 1-indexed = # of completed epochs.
+    save_path = os.path.join(self.save_path_base, f"epoch-{epoch+1}")
+    self.save_fn(save_path)
+
+
+def train_and_save(model,
+                   train_data,
+                   val_data,
+                   train_path,
+                   save_intermediates=False,
+                   **train_kw):
   """Run training and save model."""
   # Set up logging for TensorBoard. To view, run:
   #   tensorboard --log_dir=<train_path>/tensorboard
@@ -55,10 +87,13 @@ def train_and_save(model, train_data, val_data, train_path):
       tf.keras.callbacks.TensorBoard(
           log_dir=os.path.join(train_path, "tensorboard"))
   ]
+  if save_intermediates:
+    keras_callbacks.append(EpochSaverCallback(train_path, save_fn=model.save))
   history = model.train(
       train_data.examples,
       validation_inputs=val_data.examples,
-      keras_callbacks=keras_callbacks)
+      keras_callbacks=keras_callbacks,
+      **train_kw)
 
   # Save training history too, since this is human-readable and more concise
   # than the TensorBoard log files.
@@ -66,7 +101,6 @@ def train_and_save(model, train_data, val_data, train_path):
     # Use LIT's custom JSON encoder to handle dicts containing NumPy data.
     fd.write(serialize.to_json(history_to_dict(history), simple=True, indent=2))
 
-  # Save model weights and config files.
   model.save(train_path)
   logging.info("Saved model files: \n  %s",
                "\n  ".join(os.listdir(train_path)))
@@ -99,7 +133,13 @@ def main(_):
 
   ##
   # Run training and save model.
-  train_and_save(model, train_data, val_data, FLAGS.train_path)
+  train_and_save(
+      model,
+      train_data,
+      val_data,
+      FLAGS.train_path,
+      save_intermediates=FLAGS.save_intermediates,
+      num_epochs=FLAGS.num_epochs)
 
 
 if __name__ == "__main__":
