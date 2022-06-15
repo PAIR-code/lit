931b9c49983cf2ae9dd72d228ccecdb2d403b0ad
Aryan Chaurasia <31214277+aryan1107@users.noreply.github.com>
2022-06-15 13:52:21 -0400

    TydiWrapper

--- lit_nlp/examples/models/tydi.py
+++ lit_nlp/examples/models/tydi.py
@@ -110,7 +110,6 @@ class SummarizationWrapper(lit_model.Model):
         model_name,
         output_hidden_states=True,
         output_attentions=True)
-
     # # TODO(gehrmann): temp solution for ROUGE.
     self._scorer = rouge_scorer.RougeScorer(["rougeL"], use_stemmer=True)
     # If output is List[(str, score)] instead of just str
@@ -119,6 +118,14 @@ class SummarizationWrapper(lit_model.Model):
     self._get_pred_string = (
         lit_types.GeneratedTextCandidates.top_text if self._multi_output else
         (lambda x: x))
+
+  def _encode_texts(self, texts: List[str]):
+      return self.tokenizer.batch_encode_plus(
+          texts,
+          return_tensors="jax",
+          padding="longest",
+          truncation="longest_first")
+  
   def _detokenize(self, ids):
     tokens = self.tokenizer.convert_ids_to_tokens(ids)
     return [self.clean_bpe_token(t) for t in tokens]
@@ -146,7 +153,7 @@ class SummarizationWrapper(lit_model.Model):
     Returns:
       payload: Dictionary with items described above, each as single Tensor.
     """
-    print(encoded_inputs)
+    # print(encoded_inputs)
     out: transformers.modeling_flax_outputs.FlaxQuestionAnsweringModelOutput = \
         self.model(encoded_inputs["input_ids"])
 
@@ -167,30 +174,84 @@ class SummarizationWrapper(lit_model.Model):
           out.hidden_states[i], axis=1)
 
     return batched_outputs
+  def _force_decode(self, encoded_inputs):
+    """Get predictions for a batch of tokenized examples.
 
+    Each forward pass produces the following:
+      logits: batch_size x dec_len x vocab_size
+      decoder_past_key_value_states: tuple with cached outputs.
+      dec_states: tuple[len:dec_layers]:
+                  batch_size x dec_len x hid_size
+      dec_attn: [optional] tuple[len:dec_layers+1]
+                batch_size x num_heads x dec_len x dec_len
+      enc_final_state: batch_size x enc_len x hid_size
+      enc_states: tuple[len:enc_layers]:
+                  batch_size x enc_len x hid_size
+      enc_attn: [optional] tuple[len:enc_layers+1]
+                batch_size x num_heads x enc_len x enc_len
+
+    The two optional attention fields are only returned if
+    config.output_attention is set.
+
+    Args:
+      encoded_inputs: Dict as returned from Tokenizer for inputs.
+
+    Returns:
+      batched_outputs: Dict[str, jax]
+    """
+    results = self.model(
+        input_ids=encoded_inputs["input_ids"],
+        attention_mask=encoded_inputs["attention_mask"]
+        )
+
+    model_probs = tf.nn.softmax(results.logits, axis=-1)
+    top_k = tf.math.top_k(
+        model_probs, k=self.config.token_top_k, sorted=True, name=None)
+    batched_outputs = {
+        "input_ids": encoded_inputs["input_ids"],
+        "input_ntok": tf.reduce_sum(encoded_inputs["attention_mask"], axis=1),
+        # "target_ids": encoded_targets["input_ids"],
+        # "target_ntok": tf.reduce_sum(encoded_targets["attention_mask"], axis=1),
+        "top_k_indices": top_k.indices,
+        "top_k_probs": top_k.values,
+    }
+    # encoder_last_hidden_state is <float>[batch_size, num_tokens, emb_dim]
+    # take the mean over real tokens to get <float>[batch_size, emb_dim]
+    batched_outputs["encoder_final_embedding"] = masked_token_mean(
+        results.encoder_last_hidden_state, encoded_inputs["attention_mask"])
+
+    if self.config.output_attention:
+      for i in range(len(results.decoder_attentions)):
+        batched_outputs[
+            f"decoder_layer_{i+1:d}_attention"] = results.decoder_attentions[i]
+      for i in range(len(results.encoder_attentions)):
+        batched_outputs[
+            f"encoder_layer_{i+1:d}_attention"] = results.encoder_attentions[i]
+    print("Batched_outputs incoming.........")
+    print(batched_outputs)
+    return batched_outputs
   ##
   # LIT API implementation
   def max_minibatch_size(self) -> int:
     # The lit.Model base class handles batching automatically in the
     # implementation of predict(), and uses this value as the batch size.
-    return 8
+    return self.config.inference_batch_size
 
   def predict_minibatch(self, inputs):
     """Predict on a single minibatch of examples."""
     # If input has a 'tokens' field, use that. Otherwise tokenize the text.
+  
+    # Text as sequence of sentencepiece ID"s.
     new_input = []
     for  i in inputs:
         new_input.append(i['question']+ i['context'])
 
-    encoded_inputs = self.tokenizer.batch_encode_plus(
-        new_input,
-        return_tensors="jax",
-        add_special_tokens=True,
-        padding="longest",
-        truncation="longest_first")
+    encoded_inputs = self._encode_texts(new_input)
+    print('encoded_inputs')
+    print(encoded_inputs)
     
     # Get the predictions.
-    batched_outputs = self._pred(encoded_inputs)
+    batched_outputs = self._force_decode(encoded_inputs)
     # Convert to numpy for post-processing.
     detached_outputs = {k: v.numpy() for k, v in batched_outputs.items()}
     # Split up batched outputs, then post-process each example.
--- lit_nlp/examples/tydi_demo.py
+++ lit_nlp/examples/tydi_demo.py
@@ -113,7 +113,7 @@ def main(argv: Sequence[str]) -> Optional[dev_server.LitServerType]:
     # Ignore path prefix, if using /path/to/<model_name> to load from a
     # specific directory rather than the default shortcut.
     model_name = os.path.basename(model_name_or_path)
-    models[model_name] = tydi.SummarizationWrapper(
+    models[model_name] = tydi.TydiWrapper(
           model=model_name_or_path,
           model_name=model_name_or_path,
           num_to_generate=_NUM_TO_GEN.value,
