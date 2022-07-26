4c93b62da400ae30a86b65e415bd495f3e611449
Crystal Qian <cjqian@google.com>
2022-07-26 13:27:54 -0700

    Add boolean typing to ImageSalience's signed property.
    PiperOrigin-RevId: 463414099

--- lit_nlp/api/types.py
+++ lit_nlp/api/types.py
@@ -511,7 +511,7 @@ class ImageSalience(_Salience):
   The data is returned as an image in the base64 URL encoded format, e.g.,
   data:image/jpg;base64,w4J3k1Bfa...
   """
-  signed = False  # If the returned values are signed.
+  signed: bool = False  # If the returned values are signed.
 
 
 @attr.s(auto_attribs=True, frozen=True, kw_only=True)
