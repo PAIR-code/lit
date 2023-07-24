8fd72bf7ba8f90915be275fa17670e9aa6a5cd37
Ryan Mullins <ryanmullins@google.com>
2023-07-24 11:33:44 -0700

    Internal change.
    PiperOrigin-RevId: 550624236

--- lit_nlp/examples/models/t5_int_test.py
+++ lit_nlp/examples/models/t5_int_test.py
@@ -10,8 +10,9 @@ class T5IntTest(absltest.TestCase):
   def test_t5_predict(self):
     # Run prediction to ensure no failure.
     model_path = "t5-small"
-    model = t5.T5HFModel(model_path, num_to_generate=1, token_top_k=1,
-                         output_attention=False)
+    model = t5.T5HFModel(
+        model_path, num_to_generate=1, token_top_k=1, output_attention=False
+    )
     model_in = [{"input_text": "test text"}]
     model_out = list(model.predict(model_in))
 
