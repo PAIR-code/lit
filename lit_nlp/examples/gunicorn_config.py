b14e3b1a81d7b6305063f778f46666a4d1326045
Ryan Mullins <ryanmullins@google.com>
2024-06-25 11:13:48 -0700

    Updated gunicorn config for demos running in Docker.
    PiperOrigin-RevId: 646542614

--- lit_nlp/examples/gunicorn_config.py
+++ lit_nlp/examples/gunicorn_config.py
@@ -12,15 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Config for gunicorn for cloud-hosted demos."""
+"""gunicorn configuration for cloud-hosted demos."""
 
 import os
 
-_DEMO_NAME = os.getenv('DEMO_NAME', 'glue_demo')
+_DEMO_NAME = os.getenv('DEMO_NAME', 'glue')
 _DEMO_PORT = os.getenv('DEMO_PORT', '5432')
 
 bind = f'0.0.0.0:{_DEMO_PORT}'
 timeout = 3600
 threads = 8
 worker_class = 'gthread'
-wsgi_app = f'lit_nlp.examples.{_DEMO_NAME}:get_wsgi_app()'
+wsgi_app = f'lit_nlp.examples.{_DEMO_NAME}.demo:get_wsgi_app()'
--- lit_nlp/examples/tydi/demo.py
+++ lit_nlp/examples/tydi/demo.py
@@ -12,6 +12,7 @@ from typing import Optional
 
 from absl import app
 from absl import flags
+from absl import logging
 from lit_nlp import dev_server
 from lit_nlp import server_flags
 from lit_nlp.components import word_replacer
@@ -40,7 +41,9 @@ def get_wsgi_app() -> Optional[dev_server.LitServerType]:
   # Parse flags without calling app.run(main), to avoid conflict with
   # gunicorn command line flags.
   unused = flags.FLAGS(sys.argv, known_only=True)
-  return main(unused)
+  if unused:
+    logging.info("tydi_demo:get_wsgi_app() called with unused args: %s", unused)
+  return main([])
 
 
 def main(argv: Sequence[str]) -> Optional[dev_server.LitServerType]:
