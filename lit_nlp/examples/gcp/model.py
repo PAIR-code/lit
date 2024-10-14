110c999f3250863275b6e4a172789ef66b9589ec
Ryan Mullins <ryanmullins@google.com>
2024-10-14 19:13:21 +0000

    Adding license declarations

--- lit_nlp/examples/gcp/model.py
+++ lit_nlp/examples/gcp/model.py
@@ -1,3 +1,18 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
 """Wrapper for connetecting to LLMs on GCP via the model_server HTTP API."""
 
 from lit_nlp import app as lit_app
@@ -76,7 +91,7 @@ class LlmOverHTTP(lit_model.BatchedRemoteModel):
     """
     inputs = {'inputs': inputs}
     headers = {
-        'Authorization': f'Bearer {self.identity_token}',  
+        'Authorization': f'Bearer {self.identity_token}',
         'Content-Type': 'application/json'
     }
     response = requests.post(
