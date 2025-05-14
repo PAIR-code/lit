1f29afbf491d8567d44b32822725b8b355415894
llcourage <llcourage123@gmail.com>
2025-05-14 00:10:54 -0400

    Test and fix errors in dalle_mini examples.

--- lit_nlp/examples/dalle_mini/data.py
+++ lit_nlp/examples/dalle_mini/data.py
@@ -7,12 +7,16 @@ from lit_nlp.api import types as lit_types
 class DallePrompts(lit_dataset.Dataset):
 
   def __init__(self, prompts: list[str]):
-    self.examples = []
+    self._examples = []
     for prompt in prompts:
-      self.examples.append({"prompt": prompt})
+      self._examples.append({"prompt": prompt})
 
   def spec(self) -> lit_types.Spec:
     return {"prompt": lit_types.TextSegment()}
 
   def __iter__(self):
-    return iter(self.examples)
+    return iter(self._examples)
+
+  @property
+  def examples(self):
+    return self._examples
